20a01ba455e2dc1340d655090f2e7145892ba862
Roelan Eroy <roelan@vanana.com>
2020-09-11 11:14:07 +0800

    Initial Commit

---
+++ Chika/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chika
+//
+//  Created by Vananaz Developer on 9/11/20.
+//  Copyright © 2020 Roe Developer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
