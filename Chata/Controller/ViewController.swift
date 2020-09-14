0c8dc0be88e5c861ce290d9a11de393c32ef8d40
Roelan Eroy <roelan@vanana.com>
2020-09-14 20:53:26 +0800

    Initial commit

---
+++ Chata/Controller/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  Chika
+//
+//  Created by Vananaz Developer on 9/11/20.
+//  Copyright © 2020 Roe Developer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var textFieldEmail: UITextField!
+    @IBOutlet weak var textFieldPass: UITextField!
+    @IBOutlet weak var btnLogin: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        // //Keyboard dismiss
+//        let tapGesture = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing))
+//        view.addGestureRecognizer(tapGesture)
+//
+//        tapGesture.cancelsTouchesInView = false
+        
+        textFieldEmail.delegate = self;
+        textFieldPass.delegate = self;
+        btnLogin.roundedCorner()
+    }
+    
+
+
+}
+
+extension ViewController: UITextFieldDelegate{
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        return textField.resignFirstResponder()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+
+}
+
+extension UIButton{
+    
+    func roundedCorner(){
+        self.clipsToBounds = true;
+        self.layer.cornerRadius = 30
+    }
+}
